0d2df36503b0d83ba6e0bd10142b0773ddf047fe
Tomás <tococionnaith@gmail.com>
2026-04-14 09:13:00 +0100

    Initial commit

---
+++ # DNA Sequence Analyser.py
@@ -0,0 +1,26 @@
+# DNA Sequence Analyser
+
+
+#takes input - DNA string
+# outputs results such as:
+#  length, 
+# GC content, 
+# nucleotides, 
+# reverse complement
+
+
+dna_sequence = input ("Enter a DNA sequence, please make sure input is valid (only a,c,t or g): ")
+dna_sequence = dna_sequence.upper()  # Convert to uppercase
+dna_sequence = dna_sequence.strip()
+print (dna_sequence)
+
+
+
+
+
+
+
+
+
+
+
